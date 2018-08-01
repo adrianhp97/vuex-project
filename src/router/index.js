2715762e2cd3de7534f1a9e10ba4bf737d2ff031
Adrian Hartarto p <adrianhp100797@gmail.com>
2018-08-01 13:21:42 +0700

    test

--- src/router/index.js
+++ src/router/index.js
@@ -8,52 +8,52 @@ export default new Router({
   linkExactActiveClass: 'disabled active',
   routes: [
     {
-      path: '/vuex-project',
+      path: '',
       redirect: '/user',
       name: 'home'
     },
     {
-      path: '/vuex-project/user',
+      path: '/user',
       name: 'user',
       component: () => import('@/components/Users/Index')
     },
     {
-      path: '/vuex-project/user/:uid',
+      path: '/user/:uid',
       name: 'user.detail',
       component: () => import('@/components/Users/Detail')
     },
     {
-      path: '/vuex-project/user/:uid/album',
+      path: '/user/:uid/album',
       name: 'album',
       component: () => import('@/components/Albums/Index')
     },
     {
-      path: '/vuex-project/user/:uid/album/:id',
+      path: '/user/:uid/album/:id',
       name: 'album.detail',
       component: () => import('@/components/Albums/Detail')
     },
     {
-      path: '/vuex-project/user/:uid/album/:id/photo/:pid',
+      path: '/user/:uid/album/:id/photo/:pid',
       name: 'photos.detail',
       component: () => import('@/components/Photos/Detail')
     },
     {
-      path: '/vuex-project/user/:uid/post',
+      path: '/user/:uid/post',
       name: 'post',
       component: () => import('@/components/Posts/Index')
     },
     {
-      path: '/vuex-project/user/:uid/post/:id',
+      path: '/user/:uid/post/:id',
       name: 'post.detail',
       component: () => import('@/components/Posts/Detail')
     },
     {
-      path: '/vuex-project/user/:uid/post/create',
+      path: '/user/:uid/post/create',
       name: 'post.create',
       component: () => import('@/components/Posts/Form')
     },
     {
-      path: '/vuex-project/user/:uid/post/:id/edit',
+      path: '/user/:uid/post/:id/edit',
       name: 'post.edit',
       component: () => import('@/components/Posts/Form')
     },
